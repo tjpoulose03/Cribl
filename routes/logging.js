7e652d40526918e8709c13de4b17a54d5b6a69bd
tjpoulose03 <travis.poulose.j@gmail.com>
2022-11-11 18:23:17 -0500

    comments

--- routes/logging.js
+++ routes/logging.js
@@ -6,28 +6,22 @@ app.get("/getLogs", async (req, res) => {
     var fileName = req.query.fileName
     var latestLogs = req.query.latestLogs
     var searchString = req.query.searchString
-    console.log(fileName)
-    console.log(latestLogs)
-    console.log(searchString)
-    if (isPositiveInteger(latestLogs) || latestLogs == '' || !latestLogs) {
+    if (isPositiveInteger(latestLogs) || latestLogs == '' || !latestLogs) { //Checking to see if the latestLogs paramter is valid
         var result
         try {
             result = await getLogs(latestLogs, fileName, searchString)
-            console.log(result)
-            console.log("Number of Lines : " + result['data'].length)
-
         } catch (err) {
             result = err
         }
         res.json(result)
     } else {
-        res.send({ message: "Invalid latestLogs Value" })
+        res.send({ message: "Invalid latestLogs Value" }) //Invalid latestLog values
     }
 
 })
 async function getLogs(lastLinesLimit, filename, searchString) {
     const path = './var/log/' + filename
-    const fileCheck = findFile(path) //Ensure File exists in before proceeding
+    const fileCheck = findFile(path) //Ensure File exists in log folder before proceeding
     return new Promise((resolve, reject) => {
         if (fileCheck.data) {
             const fileSize = fs.statSync(path).size //Checking File Size to determine buffersize
@@ -42,7 +36,7 @@ async function getLogs(lastLinesLimit, filename, searchString) {
                 lastLines = getLastLines(Totallines.split(/\r?\n/), lastLinesLimit) //Once the file has been read, split the string into lines and get the required number of lines
                 if (searchString)
                     lastLines = searchLogs(lastLines, searchString) //filter out the lines to get lines that only contain the searchString
-                resolve({ message: 200, data: lastLines })
+                resolve({ message: "Success", data: lastLines })
             })
         } else {
             resolve(fileCheck)
@@ -54,11 +48,11 @@ async function getLogs(lastLinesLimit, filename, searchString) {
 
 function getLastLines(lines, lastLinesLimit) {
     try {
-        if (lastLinesLimit > lines.length)
+        if (lastLinesLimit > lines.length) // If more lines than the file can provide is specified then just send all the lines
             lastLinesLimit = lines.length
         let orderedLines = []
-        for (var i = lines.length - 1; i >= lines.length - lastLinesLimit; i--) {
-            orderedLines.push(lines[i])
+        for (var i = lines.length - 1; i >= lines.length - lastLinesLimit; i--) {// We are reading from the end of the file and keeping track of the number of lines
+            orderedLines.push(lines[i]) // We are pushing the lines in chronological order in this array
         }
         return (orderedLines)
     } catch (err) {
@@ -83,16 +77,16 @@ function findFile(path) {
 function searchLogs(lastLines, searchString) {
     let filteredLastLines = []
     for (var i = 0; i < lastLines.length; i++) {
-        if (lastLines[i].includes(searchString)) {
+        if (lastLines[i].includes(searchString)) { // For each lines requested, check whether the searchstring exits or not
             console.log(lastLines[i])
-            filteredLastLines.push(lastLines[i])
+            filteredLastLines.push(lastLines[i]) // Push the lines that contain the searchString to this array and return it
         }
     }
     return (filteredLastLines)
 }
 function isPositiveInteger(x) {
     const num = Number(x)
-    if (Number.isInteger(num) && num >= 0) {
+    if (Number.isInteger(num) && num >= 0) { //Checking to see whether the lastLines value is a postive integer more than or equal to 0
         return true
     } else {
         return false
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 4000; //Change to your port of choice here
 app.listen(port, () => {
     console.log("The server is running on port " + port)
 })
