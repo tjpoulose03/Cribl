c0f105d47f05772c7c3b65d59542bf2bb1298081
Travis <travis@lmcredit.com>
2022-11-11 09:47:04 -0500

    ready

--- routes/logging.js
+++ routes/logging.js
@@ -1,20 +1,48 @@
 const fs = require('fs')
 const express = require('express')
 const app = express()
-const readline = require('readline')
 
-app.get("/checking", async (req, res) => {
+app.get("/getLogs", async (req, res) => {
+    var fileName = 'log1.txt'
+    var latestLogs = 30
+    var searchString = '03:57:29 LMCreditAp'
     var result
     try {
-        result = await getLogs(23, 'log1.txt')
+        result = await getLogs(latestLogs, fileName, searchString)
         console.log(result)
-        console.log("Number of Lines : " + result.length)
+        console.log("Number of Lines : " + result['data'].length)
 
     } catch (err) {
         result = err
     }
     res.json(result)
 })
+async function getLogs(lastLinesLimit, filename, searchString) {
+    const path = './var/log/' + filename
+    const fileSize = fs.statSync(path).size //Checking File Size to determine buffersize
+    const bufferSize = Math.min(10000, fileSize) //For larger files take chunks of 10000 bytes otherwise take the entire file as a chunk 
+    const fileCheck = findFile(path) //Ensure File exists in before proceeding
+    return new Promise((resolve, reject) => {
+        if (fileCheck.data) {
+            const readStream = fs.createReadStream(path, { highWaterMark: bufferSize }) //Create a read stream to read the file in chunks
+            let Totallines = ''
+            let lastLines
+            readStream.on('data', (chunk) => {
+                Totallines = Totallines.concat(chunk.toString()) //For every chunk concat it to the Total lines string for parsing later
+            })
+            readStream.on('end', () => {
+                lastLines = getLastLines(Totallines.split(/\r?\n/), lastLinesLimit) //Once the file has been read, split the string into lines and get the required number of lines
+                lastLines = searchLogs(lastLines, searchString) //filter out the lines to get lines that only contain the searchString
+                resolve({ message: 200, data: lastLines })
+            })
+        } else {
+            resolve(fileCheck)
+        }
+
+
+    })
+}
+
 function getLastLines(lines, lastLinesLimit) {
     try {
         lines = lines.slice((lines.length) - lastLinesLimit, lines.length)
@@ -23,58 +51,30 @@ function getLastLines(lines, lastLinesLimit) {
         return (err)
     }
 }
-// async function getUpdatedLastLines(lastLinesLimit, filename) {
-//     return new Promise((resolve, reject) => {
-//         const path = './var/log/' + filename
-//         const fileSize = fs.statSync(path).size
-//         const bufferSize = Math.min(10000, fileSize)
-//         const readStream = fs.createReadStream(path, { highWaterMark: bufferSize })
-//         let incompleteLine = false
-//         let Totallines = []
-//         let lastLines
-//         readStream.on('data', (chunk) => {
-//             console.log("Chunk")
-//             console.log(chunk.toString())
-//             console.log("________________________")
-//             if ((chunk.toString().charAt((chunk.toString()).length - 1)) !== '\n') {
-//                 console.log("FOUND INCOMLETE LINE")
-//                 let chunkString = chunk.toString()
-//                 console.log(chunkString.charAt(chunkString.length - 1))
-//             }
-//             let chunkArray = chunk.toString().split('\n')
-//             // if (incompleteLine) {
-//             //     chunkArray[0] = chunkArray[0].concat(Totallines[Totallines.length - 1])
-//             //     Totallines.pop()
-//             //     incompleteLine = false
-//             // }
-//             Totallines = Totallines.concat(chunkArray)
-//         })
-//         readStream.on('end', () => {
-//             lastLines = getLastLines(Totallines, lastLinesLimit)
-//             resolve(lastLines)
-//         })
 
-//     })
-// }
-async function getLogs(lastLinesLimit, filename) {
-    return new Promise((resolve, reject) => {
-        let TotalLines = []
-        const path = './var/log/' + filename
-        const rl = readline.createInterface({
-            input: fs.createReadStream(path)
-        })
-        rl.on('line', (line) => {
-            TotalLines.push(line)
-        }).on('close', () => {
-            resolve(getLastLines(TotalLines, lastLinesLimit))
-        })
-    })
-}
-async function searchLogs() {
+function findFile(path) {
+    try {
+        if (fs.existsSync(path))
+            return ({ message: "File Found", data: true })
+        else
+            return ({ message: "File Not Found", data: false, })
+    }
+    catch (err) {
+        console.log(err)
+        return ({ message: err, data: false, })
+    }
 
 }
-async function doesFileExist() {
 
+function searchLogs(lastLines, searchString) {
+    let filteredLastLines = []
+    for (var i = 0; i < lastLines.length; i++) {
+        if (lastLines[i].includes(searchString)) {
+            console.log(lastLines[i])
+            filteredLastLines.push(lastLines[i])
+        }
+    }
+    return (filteredLastLines)
 }
 
 module.exports = app;
