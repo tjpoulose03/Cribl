4fd06de046dea88957580e7e1873b281bb70a962
Travis <travis@lmcredit.com>
2022-11-11 16:11:26 -0500

    paramters

--- routes/logging.js
+++ routes/logging.js
@@ -3,9 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get("/getLogs", async (req, res) => {
-    var fileName = 'log1.txt'
-    var latestLogs = 30
-    var searchString = '03:57:29 LMCreditAp'
+    var fileName = req.query.fileName
+    var latestLogs = req.query.latestLogs
+    var searchString = req.query.searchString
+    console.log(fileName)
+    console.log(latestLogs)
+    console.log(searchString)
     var result
     try {
         result = await getLogs(latestLogs, fileName, searchString)
@@ -19,11 +22,11 @@ app.get("/getLogs", async (req, res) => {
 })
 async function getLogs(lastLinesLimit, filename, searchString) {
     const path = './var/log/' + filename
-    const fileSize = fs.statSync(path).size //Checking File Size to determine buffersize
-    const bufferSize = Math.min(10000, fileSize) //For larger files take chunks of 10000 bytes otherwise take the entire file as a chunk 
     const fileCheck = findFile(path) //Ensure File exists in before proceeding
     return new Promise((resolve, reject) => {
         if (fileCheck.data) {
+            const fileSize = fs.statSync(path).size //Checking File Size to determine buffersize
+            const bufferSize = Math.min(10000, fileSize) //For larger files take chunks of 10000 bytes otherwise take the entire file as a chunk 
             const readStream = fs.createReadStream(path, { highWaterMark: bufferSize }) //Create a read stream to read the file in chunks
             let Totallines = ''
             let lastLines
@@ -32,7 +35,8 @@ async function getLogs(lastLinesLimit, filename, searchString) {
             })
             readStream.on('end', () => {
                 lastLines = getLastLines(Totallines.split(/\r?\n/), lastLinesLimit) //Once the file has been read, split the string into lines and get the required number of lines
-                lastLines = searchLogs(lastLines, searchString) //filter out the lines to get lines that only contain the searchString
+                if (searchString)
+                    lastLines = searchLogs(lastLines, searchString) //filter out the lines to get lines that only contain the searchString
                 resolve({ message: 200, data: lastLines })
             })
         } else {
